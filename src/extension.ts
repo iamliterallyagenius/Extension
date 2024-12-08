d0f1f7277b5875642b4435a04a7e7fd6574db53e
iamliterallyagenius <144167331+iamliterallyagenius@users.noreply.github.com>
2024-12-08 19:19:26 +0000

    doodoo

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('dbflow.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('weeee sel3a');
+		vscode.window.showInformationMessage('tiriwelli');
 	});
 
 	context.subscriptions.push(disposable);
